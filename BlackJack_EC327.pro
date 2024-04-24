0aab1037dc8c494196223c0cfc5a11a2b4cac500
jasonpliszak <jpliszak@bu.edu>
2024-04-24 00:07:25 -0400

    Card images added, game window created

--- BlackJack_EC327.pro
+++ BlackJack_EC327.pro
@@ -11,17 +11,25 @@ CONFIG += c++17
 SOURCES += \
     Card.cpp \
     Deck.cpp \
+    game.cpp \
     main.cpp \
     mainwindow.cpp
 
 HEADERS += \
     Card.h \
+    game.h \
     mainwindow.h
 
 FORMS += \
+    game.ui \
     mainwindow.ui
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
+
+RESOURCES += \
+    cards.qrc
+
+
--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 
 #include <QApplication>
 #include <QGraphicsScene>
-#include "Card.h"
 #include <QGraphicsView>
 
 
@@ -11,20 +10,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    //create scene
-    QGraphicsScene * scene = new QGraphicsScene();
-
-    //create a card item to put into the scene
-    Card *rect = new QGraphicsRectItem();
-    rect->setRect(0,0,100,100);
-
-    //add the rect to the scene
-    scene->addItem(rect);
-
-    //add a background
-    QGraphicsView * view  = new QGraphicsView(scene);
-
-    view->show();
 
     MainWindow w;
     w.show();
