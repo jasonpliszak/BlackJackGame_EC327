32d6f725215659886d070433fc0fb7faf2c391c0
jasonpliszak <jpliszak@bu.edu>
2024-04-24 19:48:18 -0400

    New card names

--- build/Qt_6_7_0_for_macOS-Debug/ui_mainwindow.h
+++ build/Qt_6_7_0_for_macOS-Debug/ui_mainwindow.h
@@ -12,7 +12,6 @@
 #include <QtCore/QVariant>
 #include <QtGui/QAction>
 #include <QtWidgets/QApplication>
-#include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
@@ -26,12 +25,7 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
-    QPushButton *pushButton;
-    QPushButton *pushButton_2;
-    QLabel *label;
-    QLabel *label_2;
-    QLabel *label_3;
-    QLabel *label_4;
+    QPushButton *play_button;
     QMenuBar *menubar;
     QMenu *menuBlackJack;
     QStatusBar *statusbar;
@@ -44,31 +38,10 @@ public:
         MainWindow->setStyleSheet(QString::fromUtf8("background-color: rgb(34, 129, 20);"));
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName("centralwidget");
-        pushButton = new QPushButton(centralwidget);
-        pushButton->setObjectName("pushButton");
-        pushButton->setGeometry(QRect(50, 270, 150, 40));
-        pushButton->setStyleSheet(QString::fromUtf8("color: rgb(255, 255, 255);\n"
-""));
-        pushButton_2 = new QPushButton(centralwidget);
-        pushButton_2->setObjectName("pushButton_2");
-        pushButton_2->setGeometry(QRect(310, 270, 150, 40));
-        pushButton_2->setStyleSheet(QString::fromUtf8("color: rgb(235, 255, 240);"));
-        label = new QLabel(centralwidget);
-        label->setObjectName("label");
-        label->setGeometry(QRect(310, 20, 150, 218));
-        label->setPixmap(QPixmap(QString::fromUtf8("../Downloads/Blackjack 2/images/103.gif")));
-        label->setScaledContents(true);
-        label_2 = new QLabel(centralwidget);
-        label_2->setObjectName("label_2");
-        label_2->setGeometry(QRect(60, 20, 150, 218));
-        label_2->setPixmap(QPixmap(QString::fromUtf8("../Downloads/Blackjack 2/images/102.gif")));
-        label_2->setScaledContents(true);
-        label_3 = new QLabel(centralwidget);
-        label_3->setObjectName("label_3");
-        label_3->setGeometry(QRect(100, 0, 58, 16));
-        label_4 = new QLabel(centralwidget);
-        label_4->setObjectName("label_4");
-        label_4->setGeometry(QRect(350, 0, 58, 16));
+        play_button = new QPushButton(centralwidget);
+        play_button->setObjectName("play_button");
+        play_button->setGeometry(QRect(180, 260, 150, 40));
+        play_button->setStyleSheet(QString::fromUtf8("color: rgb(235, 255, 240);"));
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName("menubar");
@@ -90,12 +63,7 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
-        pushButton->setText(QCoreApplication::translate("MainWindow", "Shuffle Start/Over", nullptr));
-        pushButton_2->setText(QCoreApplication::translate("MainWindow", "Deal Cards", nullptr));
-        label->setText(QString());
-        label_2->setText(QString());
-        label_3->setText(QCoreApplication::translate("MainWindow", "Dealer", nullptr));
-        label_4->setText(QCoreApplication::translate("MainWindow", "Player", nullptr));
+        play_button->setText(QCoreApplication::translate("MainWindow", "Play BlackJack", nullptr));
         menuBlackJack->setTitle(QCoreApplication::translate("MainWindow", "Game", nullptr));
     } // retranslateUi
 
