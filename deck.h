e83b15f54e4d73c85fc55ae7c5200bba0e1b1e06
Astridfdsdg <aflelder@gmail.com>
2024-04-24 20:54:11 -0400

    updated backend again

--- deck.h
+++ deck.h
@@ -1,4 +1,21 @@
 #ifndef DECK_H
 #define DECK_H
 
+#include "card.h"
+
+using namespace std;
+
+class card;
+
+class deck
+{
+public:
+    deck();
+    ~deck();
+    card draw();
+private:
+    card* cards;
+    int count;
+};
+
 #endif // DECK_H
