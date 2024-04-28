1ffab9d019bced004bd5c7bcc7cbe2fe791b5988
jasonpliszak <jpliszak@bu.edu>
2024-04-28 17:45:03 -0400

    ui update

--- build/Qt_6_7_0_for_macOS-Debug/moc_game.cpp
+++ build/Qt_6_7_0_for_macOS-Debug/moc_game.cpp
@@ -40,9 +40,12 @@ constexpr auto qt_meta_stringdata_CLASSGameENDCLASS = QtMocHelpers::stringData(
     "",
     "on_Hit_clicked",
     "on_Stand_clicked",
+    "on_Split_clicked",
     "delayedLose",
     "delayedTie",
-    "delayedWin"
+    "delayedWin",
+    "delayedBJW",
+    "delayedBJL"
 );
 #else  // !QT_MOC_HAS_STRINGDATA
 #error "qtmochelpers.h not found or too old."
@@ -55,7 +58,7 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSGameENDCLASS[] = {
       12,       // revision
        0,       // classname
        0,    0, // classinfo
-       6,   14, // methods
+       9,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -63,14 +66,20 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSGameENDCLASS[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,   50,    2, 0x08,    1 /* Private */,
-       3,    0,   51,    2, 0x08,    2 /* Private */,
-       4,    0,   52,    2, 0x08,    3 /* Private */,
-       5,    0,   53,    2, 0x08,    4 /* Private */,
-       6,    0,   54,    2, 0x08,    5 /* Private */,
-       7,    0,   55,    2, 0x08,    6 /* Private */,
+       1,    0,   68,    2, 0x08,    1 /* Private */,
+       3,    0,   69,    2, 0x08,    2 /* Private */,
+       4,    0,   70,    2, 0x08,    3 /* Private */,
+       5,    0,   71,    2, 0x08,    4 /* Private */,
+       6,    0,   72,    2, 0x08,    5 /* Private */,
+       7,    0,   73,    2, 0x08,    6 /* Private */,
+       8,    0,   74,    2, 0x08,    7 /* Private */,
+       9,    0,   75,    2, 0x08,    8 /* Private */,
+      10,    0,   76,    2, 0x08,    9 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -96,11 +105,17 @@ Q_CONSTINIT const QMetaObject Game::staticMetaObject = { {
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'on_Stand_clicked'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Split_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'delayedLose'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'delayedTie'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'delayedWin'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'delayedBJW'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'delayedBJL'
         QtPrivate::TypeAndForceComplete<void, std::false_type>
     >,
     nullptr
@@ -115,9 +130,12 @@ void Game::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void *
         case 0: _t->on_Deal_clicked(); break;
         case 1: _t->on_Hit_clicked(); break;
         case 2: _t->on_Stand_clicked(); break;
-        case 3: _t->delayedLose(); break;
-        case 4: _t->delayedTie(); break;
-        case 5: _t->delayedWin(); break;
+        case 3: _t->on_Split_clicked(); break;
+        case 4: _t->delayedLose(); break;
+        case 5: _t->delayedTie(); break;
+        case 6: _t->delayedWin(); break;
+        case 7: _t->delayedBJW(); break;
+        case 8: _t->delayedBJL(); break;
         default: ;
         }
     }
@@ -143,13 +161,13 @@ int Game::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 6)
+        if (_id < 9)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 6;
+        _id -= 9;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 6)
+        if (_id < 9)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 6;
+        _id -= 9;
     }
     return _id;
 }
